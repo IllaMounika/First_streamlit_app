64f0a765478d354583fcf11e533448d7e958923f
IllaMounika <111170333+IllaMounika@users.noreply.github.com>
2022-11-24 14:18:20 +0530

    Update Streamlit_app.py
    Added another header

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('My Parents New History Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🍝Omega 3 & blueberry Oatmeal')
-streamlit.text('🥦Kale,Spinach & ROcket Smoothie')
+streamlit.text('🥦Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥝🍞Avocado Toast')
+streamlit.header('🍌🍓Build Your Own Fruit Smoothie 🥝🍇')
