09177acfdd0dc40eba5f13eaefd9f72c4f252ed6
IllaMounika <111170333+IllaMounika@users.noreply.github.com>
2022-11-24 14:13:18 +0530

    Update Streamlit_app.py
    Added menus and items

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title('My Parents New History Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale,Spinach & ROcket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
